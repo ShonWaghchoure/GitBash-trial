bd45d2871f882c9e7d1551d3a18f839cb9e3b1fa
ShonWaghchoure <shonwaghchoure@gmail.com>
2020-09-25 17:41:15 +0530

    I have deleted the log1 from the file

--- sketch.js
+++ sketch.js
@@ -22,7 +22,6 @@ function setup(){
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
@@ -48,7 +47,6 @@ function draw(){
     box2.display();
     ground.display();
     pig1.display();
-    log1.display();
 
     box3.display();
     box4.display();
